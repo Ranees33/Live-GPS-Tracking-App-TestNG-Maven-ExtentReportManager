4efc971d488b597af24892df6ef20db13883c730
Ranees33 <raneesdeveloper@gmail.com>
2025-07-21 09:53:57 +0530

    Comment Chrome Firefox Driver in Baseclass

--- src/main/java/baseClass/Common_functions.java
+++ src/main/java/baseClass/Common_functions.java
@@ -10,9 +10,9 @@ import java.util.Properties;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterSuite;
