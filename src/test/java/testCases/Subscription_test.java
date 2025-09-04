23cfafba0c0f3cbcfa26216227d747e37c370e0f
Ranees33 <raneesdeveloper@gmail.com>
2025-09-04 17:27:05 +0530

    Update subscription test class 'add WebDriverWait method'

--- src/test/java/testCases/Subscription_test.java
+++ src/test/java/testCases/Subscription_test.java
@@ -1,7 +1,12 @@
 package testCases;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentTest;
@@ -10,17 +15,19 @@ import baseClass.Common_functions;
 import extentReports.ExtentReportManager;
 import pageObjects.Subscription_page;
 
+@Listeners(Listener.class)
 public class Subscription_test {
 	
 	WebDriver driver;
 	Subscription_page subscriptionpage;
 	ExtentTest test;
+	WebDriverWait wait;
 	
 	@Test(
 			groups = {"smoke"}, 
 			dependsOnGroups = "dashboard",
 			retryAnalyzer = RetryAnalyzer.class)
-	public void testSubsrciptionPage() {
+	public void testSubsrciptionPage() throws InterruptedException {
 		
 		driver = Common_functions.getDriver();
 		
@@ -38,19 +45,45 @@ public class Subscription_test {
 		test.info("Validate 'Buy Now' button on Subscription page");
 		
 		subscriptionpage.viewSubscription.click();
-		boolean isBuyNowDisplayed = subscriptionpage.premiumBuyNowButton.isDisplayed();
-		boolean isBuyNowEnabled = subscriptionpage.premiumBuyNowButton.isEnabled();
-		if (isBuyNowDisplayed && isBuyNowEnabled) {
-			test.pass("'Buy Now' button is displayed and enabled on Subscription Page");
+		boolean isBuyNowVisible = subscriptionpage.premiumBuyNowButton.isDisplayed();
+		if (isBuyNowVisible) {
+			test.pass("'Buy Now' button is displayed on Subscription Page");
 		} else {
-			test.fail("'Buy Now' button is not displayed or enabled on Subscription Page");
+			test.fail("'Buy Now' button is not displayed on Subscription Page");
 			
 			Assert.fail("'Buy Now' button is not displayed");
-			Assert.fail("'Buy Now' button is not enabled");
+		}
+		
+		// Verify the Buy Now button that is clickable or not to users
+		test.info("Verify the 'Buy Now' button is clickable to users");
+		
+		// boolean isBuyNowEnabled = subscriptionpage.premiumBuyNowButton.isEnabled();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		boolean isBuyNowEnabled = wait.until(ExpectedConditions.elementToBeClickable(subscriptionpage.premiumBuyNowButton)).isEnabled();
+		if (isBuyNowEnabled) {
+			test.pass("'Buy Now' button is enabled on Subscription Page");
+			subscriptionpage.premiumBuyNowButton.click();
+			Thread.sleep(4000);
+			test.pass("Buy Now button is clickable");
+			
+//			try {
+//				System.out.println(driver.getCurrentUrl());
+//				
+//			} catch (Exception e) {
+//				System.out.println("Error occured: " + e.getMessage());
+//				e.printStackTrace();
+//			}
+			
+		} else {
+			test.fail("'Buy Now' button is enabled but not clickable on Subscription Page");
+			
+			Assert.fail("Buy Now button is not clickable");
+
 		}
 		
 		test.info("Subscription page validation test passed");
 		System.out.println("Executing Test: testSubsrciptionPage | Thread ID: " + Thread.currentThread().getId());
+		
 		
 	}
 
